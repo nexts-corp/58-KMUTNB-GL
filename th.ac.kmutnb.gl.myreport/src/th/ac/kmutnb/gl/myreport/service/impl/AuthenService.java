db23c7daddc085355c879059411308520fa3ef47
Pongpanot Tassaneeyachumphalee <pongpanot.t@baanprogram.com>
2015-09-16 14:42:59 +0700

    updata databases

--- th.ac.kmutnb.gl.myreport/src/th/ac/kmutnb/gl/myreport/service/impl/AuthenService.java
+++ th.ac.kmutnb.gl.myreport/src/th/ac/kmutnb/gl/myreport/service/impl/AuthenService.java
@@ -5,8 +5,12 @@
  */
 package th.ac.kmutnb.gl.myreport.service.impl;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import th.ac.kmutnb.gl.myreport.entity.User;
 import th.co.bpg.cde.aaa.CAccount;
 import th.co.bpg.cde.aaa.CIAuthentication;
@@ -27,21 +31,40 @@ public class AuthenService extends CServiceBase implements CIAuthentication {
 
     @Override
     public CAccount signin(String Username, String Password) {
-       
-         String sql = "SELECT * FROM MASTER3D.SYSUSER WHERE DBLOGIN=?1 AND PWD=?2";
-        List<User> us = (List<User>) this.dbcon.nativeQuery(User.class, sql,Username,Password);
- 
-        if (us != null && !us.isEmpty()) {
-            CAccount acc = new CAccount();
-            acc.setCode(Username);
-            acc.setName(us.get(0).getFullName());
-            acc.setDataDomain(us.get(0).getDepartmentId() + "");
-            acc.setRole(us.get(0).getGroupId() + "");
-            acc.setResources(new ArrayList<String>());
-            
-            return acc;
+        if (this.Login2DB3D(Username, Password)) {
+            String sql = "SELECT * FROM MASTER3D.SYSUSER WHERE DBLOGIN=?1 ";
+            List<User> us = (List<User>) this.dbcon.nativeQuery(User.class, sql, Username);
+
+            if (us != null && !us.isEmpty()) {
+                CAccount acc = new CAccount();
+                acc.setCode(Username);
+                acc.setName(us.get(0).getFullName());
+                acc.setDataDomain(us.get(0).getDepartmentId() + "");
+                acc.setRole(us.get(0).getGroupId() + "");
+                acc.setResources(new ArrayList<String>());
+
+                return acc;
+            }
         }
         return null;
+    }
+
+    public boolean Login2DB3D(String userName, String password) {
+        try {
+            Connection conn = null;
+            Properties connectionProps = new Properties();
+            connectionProps.put("user", userName);
+            connectionProps.put("password", password);
+            Class.forName("oracle.jdbc.OracleDriver");
+            //DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+
+            conn = DriverManager.getConnection("jdbc:oracle:thin:@25.32.200.27:1521:gl3d", connectionProps);
+
+            conn.close();
+            return true;
+        } catch (ClassNotFoundException | SQLException ex) {
+            return false;
+        }
     }
 
     @Override
